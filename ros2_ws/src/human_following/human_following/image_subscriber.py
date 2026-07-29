ba147021ccfed70d3f338d1ccd2409afbc5ee725
장준환 <el06155@naver.com>
2026-07-29 18:21:04 +0900

    analyze YOLO bounding box coordinates

--- ros2_ws/src/human_following/human_following/image_subscriber.py
+++ ros2_ws/src/human_following/human_following/image_subscriber.py
@@ -1,4 +1,5 @@
 import cv2
+from matplotlib.pyplot import box
 import rclpy
 
 from cv_bridge import CvBridge
@@ -41,6 +42,31 @@ class ImageSubscriber(Node):
                 classes=[0],
                 verbose=False
             )
+
+            boxes = results[0].boxes
+
+            if len(boxes) > 0:
+
+                box = boxes[0]
+
+                x1, y1, x2, y2 = box.xyxy[0]
+
+                confidence = float(box.conf[0])
+
+                class_id = int(box.cls[0])
+
+                self.get_logger().info(
+                    f"x1={x1:.1f}, y1={y1:.1f}, "
+                    f"x2={x2:.1f}, y2={y2:.1f}"
+                )
+
+                self.get_logger().info(
+                    f"Confidence={confidence:.2f}"
+                )
+
+                self.get_logger().info(
+                    f"Class={class_id}"
+                )
 
             annotated_image = results[0].plot()
 

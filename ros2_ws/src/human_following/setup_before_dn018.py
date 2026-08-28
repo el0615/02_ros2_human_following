f5b514f1dfa2e29287e9623154e383e3dda75f7d
장준환 <el06155@naver.com>
2026-08-28 16:13:14 +0900

    add launch integration and final project validation

--- ros2_ws/src/human_following/setup_before_dn018.py
+++ ros2_ws/src/human_following/setup_before_dn018.py
@@ -1,6 +1,3 @@
-from glob import glob
-import os
-
 from setuptools import find_packages, setup
 
 package_name = 'human_following'
@@ -13,10 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-	 (
-            os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')
-         ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
